192e506425181365263c71d63d4834d436e01c42
DoanTrinh1213 <144203427+DoanTrinh1213@users.noreply.github.com>
2023-10-04 14:29:34 +0700

    Delete BTTH02 directory

--- BTTH02/admin/processCategory.php
+++
@@ -1,8 +0,0 @@
-<?php 
-        include '../config/connect.php';
-        $sql="SELECT * FROM theloai";
-        $stmt = $conn->prepare($sql);
-        $stmt ->execute();
-        $result = $stmt ->fetchAll();  
-
-    ?>
